cf81d6bacc394f082c485b87c7448a82ed67235d
hanumoka <amagramer@gmail.com>
2021-08-15 18:30:08 +0900

    리액트에서 form image upload 성공

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,13 @@
 import React from "react";
+import UploadForm from "./components/UploadForm";
 
 const App = () => {
-  return <h2>사진첩</h2>;
+  return (
+    <div>
+      <h2>사진첩</h2>
+      <UploadForm />
+    </div>
+  );
 };
 
 export default App;

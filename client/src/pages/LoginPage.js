2bb87553b6865953f8ab917199d646036ce08555
hanumoka <amagramer@gmail.com>
2021-08-30 12:59:55 +0900

    react-router-dom 추가

---
+++ client/src/pages/LoginPage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const LoginPage = () => {
+  return <h3>로그인페이지</h3>;
+};
+
+export default LoginPage;
---
+++ client/src/pages/RegisterPage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const RegisterPage = () => {
+  return <h3>회원가입페이지</h3>;
+};
+
+export default RegisterPage;
